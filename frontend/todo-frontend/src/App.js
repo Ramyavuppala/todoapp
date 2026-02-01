9bc0da2f245c33845d4ba417cdca2376a5335db1
Ramya <23211a12h2@bvrit.ac.in>
2026-02-01 21:45:20 +0530

    Task 4: React frontend setup and routing

---
+++ frontend/todo-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import EditTodo from "./pages/EditTodo";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/edit/:id" element={<EditTodo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
