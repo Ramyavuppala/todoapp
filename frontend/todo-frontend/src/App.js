de50b5ee4b68b44d7e89e796c6cd0d4a7ccf6710
Ramya <23211a12h2@bvrit.ac.in>
2026-02-02 20:12:09 +0530

    Task 5: Frontend-backend integration and create todo feature

--- frontend/todo-frontend/src/App.js
+++ frontend/todo-frontend/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditTodo from "./pages/EditTodo";
@@ -15,4 +14,5 @@ function App() {
 }
 
 export default App;
+
 
--- frontend/todo-frontend/src/pages/Home.js
+++ frontend/todo-frontend/src/pages/Home.js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { getTodos } from "../api/todoApi";
+
 function Home() {
-  return <h2>Todo Home Page</h2>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodos()
+      .then((res) => {
+        setTodos(res.data);   // 👈 backend → frontend
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Todo Home Page</h2>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
+
+
